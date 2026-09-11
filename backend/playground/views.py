903067ee0d6fafe614e86569ab57cdb920ec97a6
KD Leaker <shakibzeidi0@gmail.com>
2026-09-11 23:33:57 +0330

    Django debug toolbar added + practicing orm - 2

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Order
 
 def say_hello(request):
-    queryset = Product.objects.filter(description__isnull=True)
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set').order_by('-placed_at')[:5]
 
     return render(request, 'index.html', { 
         'name': 'Shakib', 
-        'products': list(queryset) 
+        'orders': list(queryset) 
     })
